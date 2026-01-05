56700f3769b3894abc9642130f2e3919615a28c3
Siddhesh Alhat <siddesh5432alhat@gmail.com>
2026-01-05 21:05:06 +0530

    first commit

---
+++ Spting-UrlShortener/src/main/java/com/one/urlshortener/SptingUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.one.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptingUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ Spting-UrlShortener/src/main/java/com/one/urlshortener/controller/UrlController.java
@@ -0,0 +1,5 @@
+package com.one.urlshortener.controller;
+
+public class UrlController {
+
+}
